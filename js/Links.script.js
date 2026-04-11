5ac32e7ecc92beca9d5cb3fd8a8bf3b11545148c
H.R <hr9084393@gmail.com>
2026-04-11 23:22:00 +0200

    Update Links.script.js

--- js/Links.script.js
+++ js/Links.script.js
@@ -1,4 +1,3 @@
-document.addEventListener('contextmenu', event => event.preventDefault());
 /**
  * وظيفة تبديل وضع الألوان (Dark Mode / Light Mode)
  * تقوم بإضافة أو إزالة كلاس "light" من عنصر الـ body
